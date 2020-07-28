9f1518fba5328da781f2bddb94fef304dc9a0628
Chiser Alexandru <chiseralexx@gmail.com>
2020-07-28 16:50:34 +0300

    Style guide PEP 8
    - changed all constants name to uppercase letters
    - changed function variables to emphasize their usage
    - placed in main all code that runs outside the defined functions

--- Messenger_Archive_Deletion.py
+++ Messenger_Archive_Deletion.py
@@ -1,7 +1,7 @@
 #Delete all archived messages from Messenger.com
-#Date: 26/07/2020
+#Date: 28/07/2020
 #Developer: Alexandru Chiser
-#Revision: 1
+#Revision: 2
 
 import time
 import pynput
@@ -11,71 +11,72 @@ from selenium.common.exceptions import ElementClickInterceptedException
 from pynput.keyboard import Key, Controller
 
 #xpaths for different interactable elements
-xpath_email = '//*[@id="email"]'
-xpath_password = '//*[@id="pass"]'
-xpath_signin = '//*[@id="loginbutton"]'
-xpath_settings = '//*[@id="settings"]'
-xpath_3dots = '//*[@id="dots-3-horizontal"]'
-xpath_delete = '//span[text()="Delete"]'
+XPATH_EMAIL = '//*[@id="email"]'
+XPATH_PASSWORD = '//*[@id="pass"]'
+XPATH_SIGNIN = '//*[@id="loginbutton"]'
+XPATH_SETTINGS = '//*[@id="settings"]'
+XPATH_3DOTS = '//*[@id="dots-3-horizontal"]'
+XPATH_DELETE = '//span[text()="Delete"]'
 
 #credentials of your Facebook account
-email = "insert email"
+email = "insert email or phone number"
 password = "insert password"
 
-#types Y string in the X text box
-def type_credentials (x, y):
-    text_box = browser.find_element_by_xpath(x)
+#types a string in a desired text box
+def type_credentials (xpath_elem, credentials):
+    text_box = browser.find_element_by_xpath(xpath_elem)
     text_box.click()
-    text_box.send_keys(y)
+    text_box.send_keys(credentials)
 
 #clicks on object X
-def click_obj (x):
-    obj = browser.find_element_by_xpath(x)
-    obj.click()     
-           
-browser = webdriver.Chrome(r"insert path to chromedriver.exe")
-url = "http://messenger.com"
-browser.get(url)
-time.sleep(2)
-
-type_credentials(xpath_email, email)
-type_credentials(xpath_password, password)
-
-click_obj(xpath_signin)
-time.sleep(2)
-
-url = "https://www.messenger.com/archived/t/"
-browser.get(url)
-time.sleep(4)
-
-#i is the number of deleted messages
-i = 0
-
-while(1):
-    try:   
-        click_obj(xpath_3dots)
-        time.sleep(2)
-    except ElementClickInterceptedException:
-        break                     
+def click_obj (xpath_elem):
+    obj = browser.find_element_by_xpath(xpath_elem)
+    obj.click()
     
-    click_obj(xpath_delete)
+if __name__ == '__main__':           
+    browser = webdriver.Chrome(r"insert path to chromedriver.exe")
+    url = "http://messenger.com"
+    browser.get(url)
     time.sleep(2)
 
-    keyboard = Controller()
+    type_credentials(XPATH_EMAIL, email)
+    type_credentials(XPATH_PASSWORD, password)
 
-    #simulate TAB key press to switch to "Delete" button
-    keyboard.press(Key.tab)
-    keyboard.release(Key.tab)
+    click_obj(XPATH_SIGNIN)
     time.sleep(2)
 
-    #simulate ENTER key press to delete the message
-    keyboard.press(Key.enter)
-    keyboard.release(Key.enter)
-    time.sleep(2)
+    url = "https://www.messenger.com/archived/t/"
+    browser.get(url)
+    time.sleep(4)
 
-    i += 1
+    #i is the number of deleted messages
+    i = 0
 
-print("{}".format(i) + " messages deleted")
+    while(1):
+        try:   
+            click_obj(XPATH_3DOTS)
+            time.sleep(2)
+        except ElementClickInterceptedException:
+            break                     
+        
+        click_obj(XPATH_DELETE)
+        time.sleep(2)
 
-browser.quit()
+        keyboard = Controller()
+
+        #simulate TAB key press to switch to "Delete" button
+        keyboard.press(Key.tab)
+        keyboard.release(Key.tab)
+        time.sleep(2)
+
+        #simulate ENTER key press to delete the message
+        keyboard.press(Key.enter)
+        keyboard.release(Key.enter)
+        time.sleep(2)
+
+        i += 1
+
+    print("{}".format(i) + " messages deleted")
+
+    browser.quit()
 
